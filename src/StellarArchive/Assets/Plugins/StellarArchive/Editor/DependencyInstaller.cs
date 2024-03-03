913bbe4f57eeec8401f42bdc70d9845fba09b03a
Hoyeom <korehdrb@gmail.com>
2024-03-03 18:19:31 +0900

    [#Feat] Auto Create Scriptable Group

--- src/StellarArchive/Assets/Plugins/StellarArchive/Editor/DependencyInstaller.cs
+++ src/StellarArchive/Assets/Plugins/StellarArchive/Editor/DependencyInstaller.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
 using UnityEditor.PackageManager;
 using UnityEngine;
 
@@ -11,34 +14,49 @@ namespace StellarArchive.Editor
     {
         static DependencyInstaller()
         {
-            var request = Client.List();
-            while (!request.IsCompleted) {}
+            TryInstallPackage("com.unity.addressables");
+            CreateScriptableObject();
+        }
+        
+    
 
-            if (request.Status == StatusCode.Success)
+        private static void TryInstallPackage(string packageName)
+        {
+            if (!IsPackageInstalled(packageName))
             {
-                bool isAddressablesInstalled = false;
-                foreach (var package in request.Result)
-                {
-                    if (package.name == "com.unity.addressables")
-                    {
-                        isAddressablesInstalled = true;
-                        break;
-                    }
-                }
-
-                if (!isAddressablesInstalled)
-                {
-                    Client.Add("com.unity.addressables");
-                    Debug.Log("패키지 생성");
-                }
-            }
-            else if (request.Status >= StatusCode.Failure)
-            {
-                Debug.Log("패키지 목록을 가져오는 데 실패했습니다.");
+                InstallPackage(packageName);
             }
             
-            // CreateScriptableObject();
+            bool IsPackageInstalled(string packageName)
+            {
+                var request = Client.List();
+                while (!request.IsCompleted) {}
+
+                if (request.Status == StatusCode.Success)
+                {
+                    foreach (var package in request.Result)
+                    {
+                        if (package.name == packageName)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                else if (request.Status >= StatusCode.Failure)
+                {
+                    Debug.Log("패키지 목록을 가져오는 데 실패했습니다.");
+                }
+
+                return false;
+            }
+            
+            void InstallPackage(string packageName)
+            {
+                Client.Add(packageName);
+                Debug.Log($"{packageName} 패키지가 설치되었습니다.");
+            }
         }
+
 
         private static void CreateScriptableObject()
         {
@@ -54,8 +72,44 @@ namespace StellarArchive.Editor
                 AssetDatabase.CreateAsset(asset, path);
                 AssetDatabase.SaveAssets();
 
-                Debug.Log("LoadingSetting ScriptableObject 생성됨.");
+                Debug.Log("LoadingSetting ScriptableObject가 생성되었습니다.");
             }
+#if STELLARARCHIVE_ADDRESSABLE_SUPPORT
+            string assetPath = AssetDatabase.GetAssetPath(asset);
+            var settings = AddressableAssetSettingsDefaultObject.Settings;
+            var groupName = nameof(StellarArchive);
+            TryCreateNewGroup(groupName, out var group);
+
+            if (settings.FindAssetEntry(AssetDatabase.AssetPathToGUID(assetPath)) == null)
+            {
+                AddressableAssetEntry entry = settings.CreateOrMoveEntry(AssetDatabase.AssetPathToGUID(assetPath), group);
+                entry.address = Path.GetFileNameWithoutExtension(assetPath);
+                settings.SetDirty(AddressableAssetSettings.ModificationEvent.EntryMoved, entry, true);
+                AssetDatabase.SaveAssets();
+            }
+#endif
         }
+        
+        
+        
+#if STELLARARCHIVE_ADDRESSABLE_SUPPORT
+        public static bool TryCreateNewGroup(string groupName, out AddressableAssetGroup group)
+        {
+            AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
+
+            if (settings.FindGroup(groupName) != null)
+            {
+                Debug.Log($"Addressable Asset Group '{groupName}' already exists.");
+                group = settings.FindGroup(groupName);
+                return false;
+            }
+
+            group = settings.CreateGroup(groupName, false, false, true, null);
+
+            Debug.Log($"Addressable Asset Group '{groupName}' created.");
+            return true;
+        }
+#endif
+
     }
 }
